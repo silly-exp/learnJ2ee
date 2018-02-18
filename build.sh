77b3bd13073484c2cf5bbbea855101761365f588
Nicolas Viel <le.nicolas.viel@gmail.com>
2018-02-18 22:36:43 +0100

    remise en ordre du code et construction du war avec Maven. Cependant ça ne fonctionne toujours pas...

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 #FIXME: to define correctly the root dir of the projet.
-rootDir="."
+#rootDir="."
 #export CLASSPATH=/usr/share/java
 #rm *.class
 #javac Demo.java
 #javac -cp ".:/usr/share/java/*" WEB-INF/src/org/happy/happytree/HelloWorld.java -d WEB-INF/classes/org/happy/happytree
-javac -cp ".:/usr/share/java/*" WEB-INF/src/org/happy/happytree/HelloWorld.java -d WEB-INF/classes
-javac -cp ".:/usr/share/java/*" WEB-INF/src/org/happy/happytree/*.java -d WEB-INF/classes
-rm -fr javadoc
+#javac -cp ".:/usr/share/java/*" WEB-INF/src/org/happy/happytree/HelloWorld.java -d WEB-INF/classes
+#javac -cp ".:/usr/share/java/*" WEB-INF/src/org/happy/happytree/*.java -d WEB-INF/classes
+#rm -fr javadoc
 #mkdir javadoc
 #javadoc -cp ".:/usr/share/java/*" $rootDir/WEB-INF/src/org/happy/happytree/* -d javadoc
+
+mvn package
--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@
 APP_PATH="/var/lib/tomcat8/webapps"
 
 rm -fr  $APP_PATH/nico
-mkdir $APP_PATH/nico
-cp -r WEB-INF $APP_PATH/nico
+#mkdir $APP_PATH/nico
+#cp -r WEB-INF $APP_PATH/nico
+cp target/happyTree.war $APP_PATH
